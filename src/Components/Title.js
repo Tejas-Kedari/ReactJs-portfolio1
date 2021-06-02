327e99ceea75c4e08a8d7d67bbc2f3a7ef59f51e
Tejas Kedari <tejaskedari8055@gmail.com>
2021-06-02 19:21:46 +0530

    Finishing the about page

---
+++ src/Components/Title.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import styled from 'styled-components'
+
+function Title({title, span}) {
+    return (
+        <div>
+            <h2>{title} <span>{span}</span></h2>
+        </div>
+    )
+}
+
+const TitleStyled = styled.div`
+    position: relative;
+    h2 {
+        color: var(--white-color);
+        font-size: 3.8rem;
+        font-weight: 600;
+        text-transform: uppercase;
+
+        span{
+            font-weight: 900;
+            color: rgba(25, 29, 43, .44);
+            font-size: 5rem;
+        }
+    }
+`;
+
+
+export default Title
